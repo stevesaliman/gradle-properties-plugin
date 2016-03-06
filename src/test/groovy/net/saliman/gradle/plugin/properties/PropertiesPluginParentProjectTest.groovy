ab85f59f968311004c0daec8a595e8ff8df15b94
Steven C. Saliman <support@saliman.net>
2016-03-06 09:46:17 -0700

    Bumped the copyright dates, now that we're in 2016

--- src/test/groovy/net/saliman/gradle/plugin/properties/PropertiesPluginParentProjectTest.groovy
+++ src/test/groovy/net/saliman/gradle/plugin/properties/PropertiesPluginParentProjectTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 Steven C. Saliman
+ * Copyright 2012-2016 Steven C. Saliman
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
